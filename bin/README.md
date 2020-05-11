050f87437cfcec282520b56de24fe42cc32f7dc1
Vyacheslav Demyanov <demyanovs@gmail.com>
2020-05-11 21:10:33 +0300

    Initial commit

---
+++ bin/README.md
@@ -0,0 +1,16 @@
+# Cauldron overflow
+
+### Install symfony cli
+``
+wget https://get.symfony.com/cli/installer -O - | bash
+``  
+
+### To start a web server  
+``
+symfony serve -d
+``  
+or  
+``
+php -S 127.0.0.1:8000 -t public/
+``
+
---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('wer');
+    }
+
+    /**
+     * @Route("/questions/{slug}")
+     */
+    public function show($slug)
+    {
+        $answers = [
+            'answer 1',
+            'answer 2',
+            'answer 3',
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers
+        ]);
+    }
+
+}
